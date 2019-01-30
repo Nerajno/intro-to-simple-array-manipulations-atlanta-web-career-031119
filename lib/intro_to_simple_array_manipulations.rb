ee1e7b0c5c8d4e01cd5bd425750d2fc106cc07c2
active-object-6703 <nerajno@gmail.com>
2019-01-30 11:35:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,14 @@ def using_shift(array)
   element = array.shift
   return element
 end
+
+def shift_with_args(array)
+  elements = array.shift(2)
+  return elements
+end
+
+def using_concat(array1,array2)
+  array1 = array1.concat(array2)
+  return array1
+end
   
